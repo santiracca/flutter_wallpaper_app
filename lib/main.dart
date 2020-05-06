c5372f3019e70d572dbe953c4ac7e7c65418b718
santiracca <santiagoracca7@gmail.com>
2020-05-06 18:01:33 -0400

    Add Firebase cloud messaging

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:wally_app/config/config.dart';
 import 'package:wally_app/screens/home_screen.dart';
@@ -30,6 +31,61 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+
+  @override
+  void initState() {
+    _firebaseMessaging.configure(
+        onMessage: (Map<String, dynamic> message) async {
+      print("OnMessage: $message");
+      String title = message["notification"]['title'] ?? "";
+      String body = message["notification"]['body'] ?? "";
+      _showDialog(title: title, body: body);
+    }, onLaunch: (Map<String, dynamic> message) async {
+      print("onLaunch: $message");
+      String title = message["notification"]['title'] ?? "";
+      String body = message["notification"]['body'] ?? "";
+      _showDialog(title: title, body: body);
+    }, onResume: (Map<String, dynamic> message) async {
+      print("onResume: $message");
+      String title = message["notification"]['title'] ?? "";
+      String body = message["notification"]['body'] ?? "";
+      _showDialog(title: title, body: body);
+    });
+
+    _firebaseMessaging.subscribeToTopic("promotion");
+
+    super.initState();
+  }
+
+  void _showDialog({String title, String body}) {
+    showDialog(
+        context: context,
+        builder: (ctx) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(80),
+            ),
+            title: Text(title),
+            content: Text(body),
+            actions: <Widget>[
+              RaisedButton(
+                onPressed: () {
+                  Navigator.of(ctx).pop();
+                },
+                child: Text('Dismiss'),
+              )
+            ],
+          );
+        });
+  }
+
+  @override
+  void dispose() {
+    _firebaseMessaging.unsubscribeFromTopic("promotion");
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
